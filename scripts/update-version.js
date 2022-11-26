fc57516e0c879a7974a56ef42bf60a0bcab24f46
fansoly <251115151@qq.com>
2022-11-26 17:27:08 +0800

    chore: init mini build

--- scripts/update-version.js
+++ scripts/update-version.js
@@ -1,6 +1,7 @@
 import path from 'path'
 import { writeFile } from 'fs/promises'
 import consola from 'consola'
+import chalk from 'chalk'
 import { getPackageManifest, getWorkspacePackages, vcOutput, vmRoot } from '@vitamin/build-utils'
 
 let shouldUpdate = true
@@ -23,13 +24,15 @@ function getVersion() {
 const version = getVersion()
 
 async function main() {
-  consola.info(`🚀 ™ Version: ${version}`)
+  consola.log(`🚀 ™ Version: ${version}`)
   if (!shouldUpdate) return
 
   await writeFile(
     path.resolve(vmRoot, 'version.js'),
     `export const version = '${version}'\n`
   )
+
+  consola.debug(chalk.yellow(`Updating package.json for vitamin-weixin`))
 
   const pkgs = Object.fromEntries(
     (await getWorkspacePackages()).filter(pkg => !!pkg.manifest.name).map((pkg) => [pkg.manifest.name, pkg])
